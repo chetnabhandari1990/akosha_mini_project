83ee06085412cdf1259e363f5c90d98f371a54ed
chetnabhandari1990 <chetnabhandari1990@gmail.com>
2014-04-10 16:05:33 +0530

    Saving replies to posts in db and UI changes
    Replies saved with corresponding posts in db
    UI changes to buttons and pages

---
+++ AKOSHA/comment.php
@@ -0,0 +1,16 @@
+﻿<?php
+//$comment_post_id='1410135859256991';
+//$comment_post_message='HEYY';
+//echo $comment_post_message.$comment_post_id;
+$comment_post_id=$_GET['post_id'];
+$comment_post_message=$_GET['post'];
+$connect =mysql_connect("localhost","root","password") or die("Couldn't connect.");
+//$sub = $_POST[0];
+mysql_select_db("akosha",$connect) or die("Couldn't find database");
+
+$insert_db=mysql_query("UPDATE akosha_post SET comment= '$comment_post_message' WHERE post_id='$comment_post_id'");
+
+//$inserting_db=mysql_query("UPDATE akosha_post (comment) VALUES ('$comment_post_message') WHERE post_id='$comment_post_id'");
+
+mysql_close($connect);
+?>
--- AKOSHA/fetch_posts.php
+++ AKOSHA/fetch_posts.php
@@ -6,7 +6,7 @@ $connect =mysql_connect("localhost","root","password") or die("Couldn't connect.
 mysql_select_db("akosha",$connect) or die("Couldn't find database");
 
 $i=0;
-$result=mysql_query("SELECT type,message,post_id,page_id,created_time FROM akosha_post WHERE user_id='$user_id' ORDER BY created_db_time DESC");
+$result=mysql_query("SELECT type,message,post_id,page_id,created_time,comment FROM akosha_post WHERE user_id='$user_id' ORDER BY created_db_time DESC");
 $key=array();
 while($row = mysql_fetch_array($result))
 	{
@@ -14,7 +14,9 @@ while($row = mysql_fetch_array($result))
 		$array = [
 		"message" => $row['message'],
 		"type" => $row['type'],
-		"post_id_full" => $row['page_id']."_".$row['post_id']
+		"post_id_full" => $row['page_id']."_".$row['post_id'] ,
+		"post_id" => $row['post_id'],
+		"comment" => $row['comment']
 		];
 		$key[$i] = $array;
 	
